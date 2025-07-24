7f36cddbe8364955765e1373f87939cd04b53de8
youngcamilz <ikediegoizuchukwu07@gmail.com>
2025-07-24 13:44:34 +0100

    Create Grading-system.py
    This Python program implements a grading system using `if` and `elif` statements. It takes a student's score as input and prints the corresponding grade based on the following scale:
    
    - 70–100: Grade A
    - 60–69: Grade B
    - 50–59: Grade C
    - 45–49: Grade D
    - 40–44: Grade E
    - 20–39: Grade F
    
    For each valid score, the program also prints "YES" to confirm the grading was successful. If the score is outside the valid range, it prints "Invalid score" and "NO".

---
+++ Grading-system.py
@@ -0,0 +1,28 @@
+# Grading System
+# Name: Ikediego Izuchukwu Camillius 
+# Reg. No: 2024704128
+# Department: [Anatomy]
+
+score = int(input("Enter your score: "))
+
+if 70 <= score <= 100:
+    print("Grade: A")
+    print("YES")
+elif 60 <= score <= 69:
+    print("Grade: B")
+    print("YES")
+elif 50 <= score <= 59:
+    print("Grade: C")
+    print("YES")
+elif 45 <= score <= 49:
+    print("Grade: D")
+    print("YES")
+elif 40 <= score <= 44:
+    print("Grade: E")
+    print("YES")
+elif 0 <= score <= 39:
+    print("Grade: F")
+    print("YES")
+else:
+    print("Invalid Score")
+    print("NO"
